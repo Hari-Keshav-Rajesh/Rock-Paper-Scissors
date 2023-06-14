5de77f02930e7b26f7438d03934d71293454d6da
Hari-Keshav-Rajesh <harikeshav.rajesh@gmail.com>
2023-06-14 12:06:52 +0530

    Bug fixed and game stops at 3 wins to one side

--- index.js
+++ index.js
@@ -13,44 +13,45 @@ function getComputerChoice(){
 
 function round(){
     let PlaySel=prompt("Enter your play(rock/paper/scissor)");
+    let a=getComputerChoice()
 
     if(PlaySel.toLowerCase()==="rock"){
-        if(getComputerChoice()==="rock"){
+        if(a==="rock"){
             console.log("It is a draw!");
         }
-        else if(getComputerChoice()==="paper"){
+        else if(a==="paper"){
             console.log("You LOSE!! Paper beats Rock.");
             ++Comp;
         }
-        else if(getComputerChoice()==="scissor"){
+        else if(a==="scissor"){
             console.log("You WIN!! Rock beats Scissor");
             ++Play;
         }
     }
 
     else if(PlaySel.toLowerCase()==="paper"){
-        if(getComputerChoice()==="paper"){
+        if(a==="paper"){
             console.log("It is a draw!");
         }
-        else if(getComputerChoice()==="scissor"){
+        else if(a==="scissor"){
             console.log("You LOSE!! Scissor beats Paper.");
             ++Comp;
         }
-        else if(getComputerChoice()==="rock"){
+        else if(a==="rock"){
             console.log("You WIN!! Paper beats Rock");
             ++Play;
         }
     }
 
     else if(PlaySel.toLowerCase()==="scissor"){
-        if(getComputerChoice()==="scissor"){
+        if(a==="scissor"){
             console.log("It is a draw!");
         }
-        else if(getComputerChoice()==="rock"){
+        else if(a==="rock"){
             console.log("You LOSE!! Rock beats Scissor.");
             ++Comp;
         }
-        else if(getComputerChoice()==="paper"){
+        else if(a==="paper"){
             console.log("You WIN!! Scissor beats Paper");
             ++Play;
         }
@@ -62,6 +63,14 @@ let Play=0;
 
 function game(){
     while((Comp+Play)<5){
+        if(Comp>2){
+            console.log("The Computer has won");
+            break;
+        }
+        if(Play>2){
+            console.log("The Player has won");
+            break;
+        }
         round();
     }
     if(Comp>Play){
