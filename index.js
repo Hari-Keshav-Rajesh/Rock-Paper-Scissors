812a75b0ba0eb06c480fe6b7f874fd5b050fe6a1
Hari-Keshav-Rajesh <harikeshav.rajesh@gmail.com>
2023-06-14 11:33:36 +0530

    Rock-Paper-Scissor 5 round game completed in console only

--- index.js
+++ index.js
@@ -9,42 +9,66 @@ function getComputerChoice(){
             return "paper";
     }
 }
-let CompSel=getComputerChoice();
-let PlaySel=prompt("Enter your play(rock/paper/scissor)");
 
-if(PlaySel.toLowerCase()==="rock"){
-    if(CompSel==="rock"){
-        console.log("It is a draw!");
+
+function round(){
+    let PlaySel=prompt("Enter your play(rock/paper/scissor)");
+
+    if(PlaySel.toLowerCase()==="rock"){
+        if(getComputerChoice()==="rock"){
+            console.log("It is a draw!");
+        }
+        else if(getComputerChoice()==="paper"){
+            console.log("You LOSE!! Paper beats Rock.");
+            ++Comp;
+        }
+        else if(getComputerChoice()==="scissor"){
+            console.log("You WIN!! Rock beats Scissor");
+            ++Play;
+        }
     }
-    if(CompSel==="paper"){
-        console.log("You LOSE!! Paper beats Rock.");
+
+    else if(PlaySel.toLowerCase()==="paper"){
+        if(getComputerChoice()==="paper"){
+            console.log("It is a draw!");
+        }
+        else if(getComputerChoice()==="scissor"){
+            console.log("You LOSE!! Scissor beats Paper.");
+            ++Comp;
+        }
+        else if(getComputerChoice()==="rock"){
+            console.log("You WIN!! Paper beats Rock");
+            ++Play;
+        }
     }
-    if(CompSel==="scissor"){
-        console.log("You WIN!! Rock beats Scissor");
+
+    else if(PlaySel.toLowerCase()==="scissor"){
+        if(getComputerChoice()==="scissor"){
+            console.log("It is a draw!");
+        }
+        else if(getComputerChoice()==="rock"){
+            console.log("You LOSE!! Rock beats Scissor.");
+            ++Comp;
+        }
+        else if(getComputerChoice()==="paper"){
+            console.log("You WIN!! Scissor beats Paper");
+            ++Play;
+        }
     }
 }
 
-if(PlaySel.toLowerCase()==="paper"){
-    if(CompSel==="paper"){
-        console.log("It is a draw!");
+let Comp=0;
+let Play=0;
+
+function game(){
+    while((Comp+Play)<5){
+        round();
     }
-    if(CompSel==="scissor"){
-        console.log("You LOSE!! Scissor beats Paper.");
+    if(Comp>Play){
+        console.log("Computer wins the game");
     }
-    if(CompSel==="rock"){
-        console.log("You WIN!! Paper beats Rock");
+    else{
+        console.log("Player wins the game");
     }
 }
-
-if(PlaySel.toLowerCase()==="scissor"){
-    if(CompSel==="scissor"){
-        console.log("It is a draw!");
-    }
-    if(CompSel==="rock"){
-        console.log("You LOSE!! Rock beats Scissor.");
-    }
-    if(CompSel==="paper"){
-        console.log("You WIN!! Scissor beats Paper");
-    }
-}
-
+game();
